1cbaded5c4637ee5201f7ef88826edbbf813a4e4
iraj munshy <munshyiraj@gmail.com>
2021-03-01 22:11:23 +0600

    football club

--- src/App.js
+++ src/App.js
@@ -4,8 +4,29 @@ import Information from './components/Information/Information';
 
 function App() {
   return (
-    <div className="container">
-      <Information></Information>
+    <div>
+      <div className="header">
+        <div>
+          <h2>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMXB56z4J50JKzzW-KvdwRjLbGEhVeDlYy1w&usqp=CAU" alt=""/>
+            LiverPool
+          </h2>
+        </div>
+
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">contact</a>
+        </nav>
+      </div>
+
+      <div className="club-name">
+        <h1>LiverPool Football Club</h1>
+      </div>
+
+      <div className="container">
+        <Information></Information>
+      </div>
     </div>
   );
 }
